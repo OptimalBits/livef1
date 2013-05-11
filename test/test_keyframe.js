3b8d50e327637fba4780325702c9836d406b3031
Manuel Astudillo <manuel@optimalbits.com>
2013-05-11 12:09:11 +0200

    Used config instead of hardcoded values

--- test/test_keyframe.js
+++ test/test_keyframe.js
@@ -1,18 +1,21 @@
 
 var livef1 = require('../livef1');
+var env = process.env;
 
 //
 // Test using pre-downloaded keyframe
 //
 
-livef1.login("manuel@optimalbits.com", "mierda350").then(function(cookie){
-  var fs = require('fs');
-  var buffer = fs.readFileSync('fixtures/keyframe_00379.bin');
+if(env.LIVEF1_USER && env.LIVEF1_PASSWD){
+  livef1.login(env.LIVEF1_USER, env.LIVEF1_PASSWD, function(packet){
+    var fs = require('fs');
+    var buffer = fs.readFileSync('fixtures/keyframe_00379.bin');
 
-  livef1.streamify(buffer);
-  livef1.parseStream(buffer, cookie, function(packet){
-    console.log(packet)
+    livef1.streamify(buffer);
+    livef1.parseStream(buffer, cookie, function(packet){
+      console.log(packet)
+    });
+  }, function(err){
+    console.log(err);
   });
-}, function(err){
-  console.log(err);
-})
+}
